7ea3a025956c45f5b3ac181e0cec309ed0e6c4fd
Kevin Trujillo <kevin.tm1994@outlook.com>
2025-10-27 17:50:22 -0400

    feat: consumir api laraver, mostrar lista de productos + categorias

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Productoservice } from '../core/services/productoservice';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
 
+  productos:any[] = [];
+  /**
+   * 
+   */
+  constructor(private productoService:Productoservice){
+    this.listarProductos();
+  }
+
+  listarProductos():void{
+    this.productoService.listaProductos().subscribe({
+      next:(data)=>{
+        this.productos = data
+        console.log(this.productos);
+      },
+      error:(err)=>console.error('error al cargar productos', err)
+    })
+  }
 }
